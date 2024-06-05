0799801e0a62871f859f21ba1b1a20a0e3e5d4d0
yamadamasashi <shisama94@gmail.com>
2024-06-05 23:59:02 +0900

    69ページ

---
+++ blog/src/components/logo.tsx
@@ -0,0 +1,6 @@
+import Link from "next/link";
+import React from "react";
+
+export const Logo = () => {
+  return <Link href="/">CIBE</Link>;
+};
---
+++ blog/src/components/nav.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/"></Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
