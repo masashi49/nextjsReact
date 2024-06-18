4afa9cc3ab900a6e338552ccd75a16c4262752ad
yamadamasashi <shisama94@gmail.com>
2024-06-19 00:39:07 +0900

    214

--- blog/src/app/blog/schedule/page.tsx
+++ blog/src/app/blog/schedule/page.tsx
@@ -3,12 +3,13 @@ import { client } from "@/lib/api";
 
 export default async function SchedulePage() {
   const data = await client.get({ endpoint: `blogs` });
-  const items = data.contents.map((item: { id: string; title: string }) => {
-    return {
-      id: item.id,
-      title: item.title,
-    };
-  });
+  const items = data.contents.map(
+    (contents: { id: string; title: string }) => ({
+      id: contents.id,
+      title: contents.title,
+    })
+  );
+
   return (
     <div>
       <h1>記事のタイトル</h1>
