136e8c843a714c460ebac665c3acad0b8a9f9964
yamadamasashi <shisama94@gmail.com>
2024-06-28 00:22:34 +0900

    ちょっと寄り道

--- blog/src/components/footer.tsx
+++ blog/src/components/footer.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
 import { Social } from "./Social";
+import { Sample } from "./sample";
 
 export const Footer = () => {
   return (
@@ -23,6 +24,8 @@ export const Footer = () => {
       <small>https://github.com/ebisucom/next-react-website/tree/main</small>
       <br />
       <small>https://nextjs-react-1wia.vercel.app/</small>
+
+      <Sample />
     </div>
   );
 };
