1f1b075a48775395cc1ae181ad8b6cc14eb0aa1d
yamadamasashi <shisama94@gmail.com>
2024-07-15 12:57:07 +0900

    269

--- blog/src/app/blog/page.tsx
+++ blog/src/app/blog/page.tsx
@@ -1,9 +1,4 @@
-import EachPost from "@/components/EachPost";
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Page() {
-  const props1 = { title: "blog", url: "/blog/schedule" };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
