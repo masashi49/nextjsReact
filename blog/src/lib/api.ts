39edecba59ed3049a9420c8098749fef9b259ab9
yamadamasashi <shisama94@gmail.com>
2024-07-24 09:35:48 +0900

    269

--- blog/src/lib/api.ts
+++ blog/src/lib/api.ts
@@ -1,48 +1,46 @@
-import { equal } from "assert";
-import { createClient } from "microcms-js-sdk";
+import { equal } from 'assert';
+import { createClient } from 'microcms-js-sdk';
 
 export interface DataType {
- id: string
- title: string
+  id: string;
+  title: string;
 }
-
-
 
 const serviceDomain = process.env.SERVICE_DOMAIN;
 const apiKey = process.env.API_KEY;
 
 if (!serviceDomain || !apiKey) {
-    throw new Error("no SERVICE_DOMAIN or apiKey");
+  throw new Error('no SERVICE_DOMAIN or apiKey');
 }
 
 export const client = createClient({
-    serviceDomain,
-    apiKey
+  serviceDomain,
+  apiKey,
 });
 
-
-
 interface Post {
-  title: string
-  subTitle : string
-  publishDate:string
-  eyecatch:{
-    url:string
-    height:number
-    width:number
-  }
-  content:string
-  categories:[]
+  title: string;
+  subTitle: string;
+  publishDate: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  content: string;
+  categories: [];
   // いくつもある
 }
 
 // type PostBySlug = Promise<Post | undefined>;
 //export const getPostBySlug: (slug: string) => PostBySlug = async (slug) => {
-export const getPostBySlug = async (slug: string): Promise<Post | undefined> => {
-    try {
+export const getPostBySlug = async (
+  slug: string
+): Promise<Post | undefined> => {
+  try {
     const post = await client.get({
       endpoint: 'blogs',
-      queries: { filters: `slug[equals]${slug}` }
+      queries: { filters: `slug[equals]${slug}` },
     });
     return post.contents[0];
   } catch (err) {
@@ -51,21 +49,41 @@ export const getPostBySlug = async (slug: string): Promise<Post | undefined> => 
   }
 };
 
-
 export type Slugs = {
-   title: string,
-   slug: string
-}
+  title: string;
+  slug: string;
+};
 
-export const getAllSlugs = async (limit:number = 100):Promise<Slugs[]|undefined> => {
-    try {
+export const getAllSlugs = async (
+  limit: number = 100
+): Promise<Slugs[] | undefined> => {
+  try {
     const slugs = await client.get({
       endpoint: 'blogs',
-      queries: { fields : "title,slug",orders:"-publisDate",limit: limit }
+      queries: { fields: 'title,slug', orders: '-publisDate', limit: limit },
     });
     return slugs.contents;
   } catch (err) {
     console.log(err);
     return undefined;
   }
-}
+};
+
+export const getAllPosts = async (
+  limit: number = 100
+): Promise<Slugs[] | undefined> => {
+  try {
+    const slugs = await client.get({
+      endpoint: 'blogs',
+      queries: {
+        fields: 'title,slug,eyecatch',
+        orders: '-publisDate',
+        limit: limit,
+      },
+    });
+    return slugs.contents;
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+};
